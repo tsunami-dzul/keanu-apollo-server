5f6225de5655176c4613b420db2a11cf817bc643
Esdras Dzul <xdzul18@gmail.com>
2024-03-04 09:58:49 -0600

    Add port config variable

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { InMemoryLRUCache } from '@apollo/utils.keyvaluecache';
 import { keanuSchema } from './schemas/keanuSchema';
 import { keanuResolver } from './resolvers/keanuResolver';
+import { config } from './config/config';
 
 const server = new ApolloServer({
+  cache: new InMemoryLRUCache(),
   typeDefs: keanuSchema,
   resolvers: keanuResolver,
 });
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: config.port },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
